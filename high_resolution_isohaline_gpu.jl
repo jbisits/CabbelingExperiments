159dad8ad108f1ad5d9a14543d90e17f60e5fd2d
Josef Bisits <[jbisits@gmail.com]>
2023-06-15 21:01:44 +1000

    Initial commit

---
+++ high_resolution_isohaline_gpu.jl
@@ -0,0 +1,33 @@
+# High resolution two layer simulation
+using DirectNumericalCabbelingShenanigans
+using DirectNumericalCabbelingShenanigans.TwoLayerDNS
+
+architecture = GPU()
+diffusivities = (ν = 1e-4, κ = (S = 1e-6, T = 1e-5))
+resolution = (Nx = 20, Ny = 20, Nz = 4000)
+reference_density = gsw_rho(S₀ˡ, T₀ˡ, 0)
+
+## Setup the model
+model = DNS(architecture, domain_extent, resolution, diffusivities; reference_density)
+
+## set initial conditions
+T₀ᵘ = -1.5
+isohaline = IsohalineUpperLayerInitialConditions(T₀ᵘ)
+initial_conditions = TwoLayerInitialConditions(isohaline)
+set_two_layer_initial_conditions!(model, initial_conditions;
+                                  perturb_salinity = true,
+                                  interface_location = 0.375, interface_thickness = 5000,
+                                  salinity_perturbation_width = 100)
+
+## random horizontal velocity noise
+#u = model.velocities.u
+#u_i = 1e-16 * rand(size(u)...)
+#set!(model, u = u_i)
+
+## build the simulation
+Δt = 1e-5
+stop_time = 2
+simulation = DNS_simulation_setup(model, Δt, stop_time, initial_conditions)
+
+## Run the simulation
+run!(simulation)
