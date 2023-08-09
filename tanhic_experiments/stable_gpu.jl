4c8471abb2aaa417ac778c0503a88e6c14235e5b
jbisits <jbisits@gmail.com>
2023-08-09 20:10:26 +1000

    Correct variable

--- tanhic_experiments/stable_gpu.jl
+++ tanhic_experiments/stable_gpu.jl
@@ -17,7 +17,7 @@ initial_conditions = TwoLayerInitialConditions(stable)
 profile_function = HyperbolicTangent(INTERFACE_LOCATION, 100)
 z = znodes(model.grid, Center(), Center(), Center())
 depth_idx = findfirst(z .> INTERFACE_LOCATION / 2)
-salinity_perturbation = GaussianBlob(depth[depth_idx], [0, 0], 1.0)
+salinity_perturbation = GaussianBlob(z[depth_idx], [0, 0], 1.0)
 set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
                                   salinity_perturbation)
 
