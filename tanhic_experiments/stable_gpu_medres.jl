a94d57c03272e3f9255bc04f4e33325cdd38608d
jbisits <jbisits@gmail.com>
2023-08-14 11:14:44 +1000

    Fix so all args are `Float64`

--- tanhic_experiments/stable_gpu_medres.jl
+++ tanhic_experiments/stable_gpu_medres.jl
@@ -18,7 +18,7 @@ initial_conditions = TwoLayerInitialConditions(stable)
 profile_function = HyperbolicTangent(INTERFACE_LOCATION, 100)
 z = znodes(model.grid, Center(), Center(), Center())
 depth_idx = findfirst(z .> 2 * INTERFACE_LOCATION / 3)
-salinity_perturbation = GaussianBlob(z[depth_idx], [0, 0], 1.0)
+salinity_perturbation = GaussianBlob(z[depth_idx], [0.0, 0.0], 1.5)
 set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
                                   salinity_perturbation)
 
