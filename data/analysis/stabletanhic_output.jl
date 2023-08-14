08fce0f797a84febd0da8c7dc8a819887779af74
jbisits <jbisits@gmail.com>
2023-08-14 15:09:20 +1000

    Update stabletanhic_output.jl

--- data/analysis/stabletanhic_output.jl
+++ data/analysis/stabletanhic_output.jl
@@ -3,7 +3,7 @@ using Oceananigans.Fields
 using DirectNumericalCabbelingShenanigans.OutputUtilities
 
 ## Load in saved output
-sim_path = joinpath(SIMULATION_PATH, "tanhic/equal_diffusivity/stable_medres_1min.jld2")
+sim_path = joinpath(SIMULATION_PATH, "tanhic/equal_diffusivity/perturb_salinity/stable.jld2")
 T_ts = FieldTimeSeries(sim_path, "T")
 S_ts = FieldTimeSeries(sim_path, "S")
 
