ee2dc6cbe15287165ff3e088cf86fbf9cd32cade
jbisits <jbisits@gmail.com>
2023-08-15 11:26:33 +1000

    Move instability closer to interface + increase steepness of change

--- tanhic_experiments/stable_gpu.jl
+++ tanhic_experiments/stable_gpu.jl
@@ -4,9 +4,10 @@ using DirectNumericalCabbelingShenanigans.TwoLayerDNS
 
 architecture = GPU()
 diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
+resolution = (Nx = 50, Ny = 50, Nz = 3000)
 
 ## Setup the model
-model = DNS(architecture, DOMAIN_EXTENT, HIGH_RESOLUTION, diffusivities;
+model = DNS(architecture, DOMAIN_EXTENT, resolution, diffusivities;
             reference_density = REFERENCE_DENSITY)
 
 ## set initial conditions
@@ -14,10 +15,10 @@ T₀ᵘ = -1.5
 S₀ᵘ = 34.551
 stable = StableUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
 initial_conditions = TwoLayerInitialConditions(stable)
-profile_function = HyperbolicTangent(INTERFACE_LOCATION, 50.0)
+profile_function = HyperbolicTangent(INTERFACE_LOCATION, 200.0)
 # z = znodes(model.grid, Center(), Center(), Center())
 # depth_idx = findfirst(z .> INTERFACE_LOCATION / 2)
-salinity_perturbation = GaussianBlob(-0.24989583333333332, [0.0, 0.0], 10.0)
+salinity_perturbation = GaussianBlob(-0.3373611111111111, [0.0, 0.0], 1.5)
 set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
                                   salinity_perturbation)
 
--- tanhic_experiments/stable_gpu_medres.jl
+++ tanhic_experiments/stable_gpu_medres.jl
@@ -15,16 +15,16 @@ T₀ᵘ = -1.5
 S₀ᵘ = 34.551
 stable = StableUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
 initial_conditions = TwoLayerInitialConditions(stable)
-profile_function = HyperbolicTangent(INTERFACE_LOCATION, 100.0)
+profile_function = HyperbolicTangent(INTERFACE_LOCATION, 200.0)
 #z = znodes(model.grid, Center(), Center(), Center())
 #depth_idx = findfirst(z .> 2 * INTERFACE_LOCATION / 3)
-salinity_perturbation = GaussianBlob(-0.24958333333333338, [0.0, 0.0], 1.5)
+salinity_perturbation = GaussianBlob(-0.33708333333333335, [0.0, 0.0], 1.5)
 set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
                                   salinity_perturbation)
 
 ## build the simulation
 Δt = 1e-5
-stop_time = 20 * 60 # seconds (in simulation time)
+stop_time = 60 # seconds (in simulation time)
 save_schedule = 1 # seconds
 simulation = DNS_simulation_setup(model, Δt, stop_time, save_schedule, initial_conditions)
 
