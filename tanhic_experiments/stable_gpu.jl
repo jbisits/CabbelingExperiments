b5850d01f0ee2c5ecda36550cb072f973ca652b2
jbisits <jbisits@gmail.com>
2023-08-16 10:58:46 +1000

    Update stable_gpu.jl

--- tanhic_experiments/stable_gpu.jl
+++ tanhic_experiments/stable_gpu.jl
@@ -4,21 +4,21 @@ using DirectNumericalCabbelingShenanigans.TwoLayerDNS
 
 architecture = GPU()
 diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
-resolution = (Nx = 50, Ny = 50, Nz = 3000)
+resolution = (Nx = 100, Ny = 100, Nz = 3000)
 
 ## Setup the model
 model = DNS(architecture, DOMAIN_EXTENT, resolution, diffusivities;
             reference_density = REFERENCE_DENSITY)
 
 ## set initial conditions
-T₀ᵘ = -1.5
-S₀ᵘ = 34.551
+T₀ᵘ = -0.5
+S₀ᵘ = 34.625
 stable = StableUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
 initial_conditions = TwoLayerInitialConditions(stable)
 profile_function = HyperbolicTangent(INTERFACE_LOCATION, 500.0)
 # z = znodes(model.grid, Center(), Center(), Center())
 # depth_idx = findfirst(z .> INTERFACE_LOCATION / 2)
-salinity_perturbation = GaussianBlob(-0.3373611111111111, [0.0, 0.0], 1.0)
+salinity_perturbation = GaussianBlob(-0.3373611111111111, [0.0, 0.0], 0.5)
 set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
                                   salinity_perturbation)
 
