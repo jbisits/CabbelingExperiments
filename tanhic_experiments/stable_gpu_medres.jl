8a4ef01f708997ba997d1c432baef819baaf2d1d
jbisits <jbisits@gmail.com>
2023-08-16 10:38:08 +1000

    Update stable_gpu_medres.jl

--- tanhic_experiments/stable_gpu_medres.jl
+++ tanhic_experiments/stable_gpu_medres.jl
@@ -18,14 +18,14 @@ initial_conditions = TwoLayerInitialConditions(stable)
 profile_function = HyperbolicTangent(INTERFACE_LOCATION, 200.0)
 #z = znodes(model.grid, Center(), Center(), Center())
 #depth_idx = findfirst(z .> 2 * INTERFACE_LOCATION / 3)
-salinity_perturbation = GaussianBlob(-0.33708333333333335, [0.0, 0.0], 1.5)
+salinity_perturbation = GaussianBlob(-0.33708333333333335, [0.0, 0.0], 1.0)
 set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
                                   salinity_perturbation)
 
 ## build the simulation
 Δt = 1e-5
-stop_time = 60 # seconds (in simulation time)
-save_schedule = 1 # seconds
+stop_time = 2 * 60 # seconds (in simulation time)
+save_schedule = 2 # seconds
 simulation = DNS_simulation_setup(model, Δt, stop_time, save_schedule, initial_conditions)
 
 ## Run the simulation
