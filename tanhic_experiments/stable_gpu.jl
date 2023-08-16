0f41dcfda4bb6c60f2a5113894838c2f9e47d749
jbisits <jbisits@gmail.com>
2023-08-16 10:55:58 +1000

    Update stable_gpu.jl

--- tanhic_experiments/stable_gpu.jl
+++ tanhic_experiments/stable_gpu.jl
@@ -24,7 +24,7 @@ set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
 
 ## build the simulation
 Δt = 1e-5
-stop_time = 10 # seconds (in simulation time)
+stop_time = 60 # seconds (in simulation time)
 save_schedule = 1 # seconds
 simulation = DNS_simulation_setup(model, Δt, stop_time, save_schedule, initial_conditions)
 
