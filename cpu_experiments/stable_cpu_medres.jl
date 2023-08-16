6ed831ba09b38813350c998f27c91ba49e1c0e23
jbisits <jbisits@gmail.com>
2023-08-17 09:48:12 +1000

    Updates to `resolution`

--- cpu_experiments/stable_cpu_medres.jl
+++ cpu_experiments/stable_cpu_medres.jl
@@ -4,7 +4,7 @@ using DirectNumericalCabbelingShenanigans.TwoLayerDNS
 
 architecture = CPU()
 diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
-resolution = (Nx = 20, Ny = 20, Nz = 1000)
+resolution = (Nx = 50, Ny = 50, Nz = 1500)
 
 ## Setup the model
 model = DNS(architecture, DOMAIN_EXTENT, resolution, diffusivities;
@@ -16,12 +16,13 @@ S₀ᵘ = 34.551
 stable = StableUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
 initial_conditions = TwoLayerInitialConditions(stable)
 profile_function = HyperbolicTangent(INTERFACE_LOCATION, 1000.0)
-#z = znodes(model.grid, Center(), Center(), Center())
-#depth_idx = findfirst(z .> 2 * INTERFACE_LOCATION / 3)
-salinity_perturbation = GaussianBlob(-0.3554166666666667, [0.0, 0.0], 1.0)
+z = znodes(model.grid, Center(), Center(), Center())
+depth_idx = findfirst(z .> 0.97 * INTERFACE_LOCATION)
+salinity_perturbation = GaussianBlob(z[depth_idx], [0.0, 0.0], 1.0)
 set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
                                   salinity_perturbation)
-
+DNCS.OutputUtilities.visualise_initial_conditions(model, 1, 1)
+DNCS.OutputUtilities.visualise_initial_density(model, 1, 1, 0)
 ## build the simulation
 Δt = 1e-5
 stop_time = 5 * 60 # seconds (in simulation time)
--- tanhic_experiments/stable_gpu_medres.jl
+++ tanhic_experiments/stable_gpu_medres.jl
@@ -4,7 +4,7 @@ using DirectNumericalCabbelingShenanigans.TwoLayerDNS
 
 architecture = GPU()
 diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
-resolution = (Nx = 20, Ny = 20, Nz = 1000)
+resolution = (Nx = 50, Ny = 50, Nz = 1500)
 
 ## Setup the model
 model = DNS(architecture, DOMAIN_EXTENT, resolution, diffusivities;
@@ -16,9 +16,8 @@ S₀ᵘ = 34.551
 stable = StableUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
 initial_conditions = TwoLayerInitialConditions(stable)
 profile_function = HyperbolicTangent(INTERFACE_LOCATION, 1000.0)
-#z = znodes(model.grid, Center(), Center(), Center())
-#depth_idx = findfirst(z .> 2 * INTERFACE_LOCATION / 3)
-salinity_perturbation = GaussianBlob(-0.3554166666666667, [0.0, 0.0], 1.0)
+# Need to find this depth manually because `GPU()` does not like scalar indexing.
+salinity_perturbation = GaussianBlob(-0.36361111111111116, [0.0, 0.0], 1.0)
 set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
                                   salinity_perturbation)
 
