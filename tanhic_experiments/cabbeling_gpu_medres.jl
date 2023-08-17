bcf9ee8eba3e3d9c5746214d8d7b04c2bf89061e
jbisits <jbisits@gmail.com>
2023-08-17 10:24:39 +1000

    Update cabbeling_gpu_medres.jl

--- tanhic_experiments/cabbeling_gpu_medres.jl
+++ tanhic_experiments/cabbeling_gpu_medres.jl
@@ -4,7 +4,7 @@ using DirectNumericalCabbelingShenanigans.TwoLayerDNS
 
 architecture = GPU()
 diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
-resolution = (Nx = 20, Ny = 20, Nz = 1000)
+resolution = (Nx = 50, Ny = 50, Nz = 1400)
 
 ## Setup the model
 model = DNS(architecture, DOMAIN_EXTENT, resolution, diffusivities;
@@ -15,10 +15,11 @@ T₀ᵘ = -1.5
 S₀ᵘ = 34.568
 cabbeling = CabbelingUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
 initial_conditions = TwoLayerInitialConditions(cabbeling)
-profile_function = HyperbolicTangent(INTERFACE_LOCATION, 100)
-set_two_layer_initial_conditions!(model, initial_conditions, profile_function)
-add_velocity_random_noise!(model, 1e-2)
-
+profile_function = HyperbolicTangent(INTERFACE_LOCATION, 1000.0)
+# Need to find this depth manually because `GPU()` does not like scalar indexing.
+salinity_perturbation = RandomPerturbations(-0.36696428571428574, 0.1)
+set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
+                                  salinity_perturbation)
 ## build the simulation
 Δt = 1e-5
 stop_time = 60
