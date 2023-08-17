2098b50cc5df1ca8158133129355c00a4af4dc01
jbisits <jbisits@gmail.com>
2023-08-17 10:20:02 +1000

    Update unstable_gpu_medres.jl

--- tanhic_experiments/unstable_gpu_medres.jl
+++ tanhic_experiments/unstable_gpu_medres.jl
@@ -4,7 +4,7 @@ using DirectNumericalCabbelingShenanigans.TwoLayerDNS
 
 architecture = GPU()
 diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
-resolution = (Nx = 20, Ny = 20, Nz = 1000)
+resolution = (Nx = 50, Ny = 50, Nz = 1400)
 
 ## Setup the model
 model = DNS(architecture, DOMAIN_EXTENT, resolution, diffusivities;
@@ -15,13 +15,15 @@ T₀ᵘ = -1.5
 S₀ᵘ = 34.59
 unstable = UnstableUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
 initial_conditions = TwoLayerInitialConditions(unstable)
-profile_function = HyperbolicTangent(INTERFACE_LOCATION, 100)
-set_two_layer_initial_conditions!(model, initial_conditions, profile_function)
-add_velocity_random_noise!(model, 1e-2)
+profile_function = HyperbolicTangent(INTERFACE_LOCATION, 1000.0)
+# Need to find this depth manually because `GPU()` does not like scalar indexing.
+salinity_perturbation = RandomPerturbations(-0.36696428571428574, 0.1)
+set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
+                                  salinity_perturbation)
 
 ## build the simulation
 Δt = 1e-5
-stop_time = 60
+stop_time = 5 * 60
 save_schedule = 1 # seconds
 simulation = DNS_simulation_setup(model, Δt, stop_time, save_schedule, initial_conditions)
 
