5c098fa9954a68286dd5599b257a6c8f385cfac7
jbisits <jbisits@gmail.com>
2023-08-22 10:44:35 +1000

    Update cabbeling_gpu.jl

--- tanhic_experiments/cabbeling_gpu.jl
+++ tanhic_experiments/cabbeling_gpu.jl
@@ -14,13 +14,29 @@ T₀ᵘ = -1.5
 S₀ᵘ = 34.568
 cabbeling = CabbelingUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
 initial_conditions = TwoLayerInitialConditions(cabbeling)
-profile_function = HyperbolicTangent(INTERFACE_LOCATION, 100.0)
-set_two_layer_initial_conditions!(model, initial_conditions, profile_function)
+profile_function = HyperbolicTangent(INTERFACE_LOCATION, 3500.0)
+
+## `GaussianBlob`
+# z = znodes(model.grid, Center(), Center(), Center())
+# depth_idx = findfirst(z .> INTERFACE_LOCATION / 2)
+# salinity_perturbation = GaussianBlob(-0.3560416666666667, [0.0, 0.0], 10.0)
+# set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
+#                                   salinity_perturbation)
+
+## `GaussianProfile` eith `RandomPerturbations`
+salinity_perturbation = GaussianProfile(INTERFACE_LOCATION, INTERFACE_LOCATION / 1.1,
+                                        100.0, 2.0)
+# z = znodes(model.grid, Center(), Center(), Center())
+# depth_idx = findfirst(z .> INTERFACE_LOCATION / 1.1)
+salinity_noise = RandomPerturbations(-0.34077380952380953, 0.1)
+set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
+                                  salinity_perturbation, salinity_noise)
+
 
 ## build the simulation
 Δt = 1e-5
-stop_time = 5
-save_schedule = 1 # seconds
+stop_time = 5 * 60
+save_schedule = 5 # seconds
 simulation = DNS_simulation_setup(model, Δt, stop_time, save_schedule, initial_conditions)
 
 ## Run the simulation
