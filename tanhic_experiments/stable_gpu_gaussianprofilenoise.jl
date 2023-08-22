2aa36c878a4c6fd63ae42aa732579907f9452eb1
jbisits <jbisits@gmail.com>
2023-08-22 13:55:46 +1000

    Smaller salinity noise

--- tanhic_experiments/stable_gpu_gaussianprofilenoise.jl
+++ tanhic_experiments/stable_gpu_gaussianprofilenoise.jl
@@ -21,7 +21,7 @@ salinity_perturbation = GaussianProfile(INTERFACE_LOCATION, INTERFACE_LOCATION /
                                         100.0, 10.0)
 # z = znodes(model.grid, Center(), Center(), Center())
 # depth_idx = findfirst(z .> INTERFACE_LOCATION / 1.1)
-salinity_noise = RandomPerturbations(-0.34077380952380953, 0.1)
+salinity_noise = RandomPerturbations(-0.34077380952380953, 0.001)
 set_two_layer_initial_conditions!(model, initial_conditions, profile_function,
                                   salinity_perturbation, salinity_noise)
 
