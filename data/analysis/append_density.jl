7e9ce24af7163f6ae740c8f01aca72edba6a3d5d
jbisits <jbisits@gmail.com>
2023-08-23 23:05:01 +1000

    Update append_density.jl

--- data/analysis/append_density.jl
+++ data/analysis/append_density.jl
@@ -2,7 +2,9 @@ using DirectNumericalCabbelingShenanigans.OutputUtilities
 
 saved_simulations = readdir(SIMULATION_PATH, join = true)
 for simulation ∈ saved_simulations
+    open_sim = jldopen(simulation)
     if "σ₀" ∉ keys(jldopen(simulation)["timeseries"])
+        close(open_sim)
         compute_density!(simulation, density_string = "σ₀")
     else
         @info "A timeseries with that symbol already exists in $simulation."
