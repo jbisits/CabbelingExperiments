6cd6be164ea31fd98fc2eb5bc6197cb6c55591fa
jbisits <jbisits@gmail.com>
2023-08-23 12:58:23 +1000

    Move data reading to separate script

---
+++ data/analysis/read_DNSoutput.jl
@@ -0,0 +1,9 @@
+using Oceananigans.Fields
+
+## Load in saved output
+sim_path = joinpath(SIMULATION_PATH, "stable.jld2")
+T_ts = FieldTimeSeries(sim_path, "T")
+S_ts = FieldTimeSeries(sim_path, "S")
+
+## Compute a density `FieldTimeSeries`
+σ₀_ts = compute_density(S_ts, T_ts)
--- data/analysis/stabletanhic_output.jl
+++ data/analysis/stabletanhic_output.jl
@@ -1,20 +1,14 @@
-using DirectNumericalCabbelingShenanigans
-using Oceananigans.Fields
 using DirectNumericalCabbelingShenanigans.OutputUtilities
-
-## Load in saved output
-sim_path = joinpath(SIMULATION_PATH, "stable.jld2")
-T_ts = FieldTimeSeries(sim_path, "T")
-S_ts = FieldTimeSeries(sim_path, "S")
 
 ## Initial snapshots
 visualise_snapshot(T_ts, "Θ (°C)", 10, 10, 11)
 visualise_snapshot(S_ts, "S (gkg⁻ꜝ)", 10, 10, 11; colormap = :haline)
+visualise_snapshot(σ₀_ts, "σ₀ (kgm⁻³)", 10, 10, 10; colormap = :dense)
 
 ## Animations (x-z)
-animate_2D_field(T_ts, "Θ (°C)", 1, 1)
-
-## Compute a density `FieldTimeSeries`
-σ₀_ts = compute_density(S_ts, T_ts)
-visualise_snapshot(σ₀_ts, "σ₀ (kgm⁻³)", 10, 10, 10; colormap = :dense)
+#  Temperatures
+animate_2D_field(T_ts, "Θ (°C)", 10, 10)
+## Salinity
+animate_2D_field(T_ts, "S (gkg⁻¹)", 10, 10; colormap = :haline)
+## Density (x-z)
 animate_2D_field(σ₀_ts, "σ₀ (kgm⁻³)", 10, 10; colormap = :dense)
