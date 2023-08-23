5293bf855366f55895485558b414dbf9d77783bf
jbisits <jbisits@gmail.com>
2023-08-23 14:46:45 +1000

    Don't read `FieldTimeSeries` to memory

--- data/analysis/animate_fields.jl
+++ data/analysis/animate_fields.jl
@@ -2,25 +2,23 @@ using DirectNumericalCabbelingShenanigans.OutputUtilities
 
 ## Animations (x-z)
 @info "Animating temperature"
-colormap = :thermal
-colorrange = extrema(T_ts)
-lowclip = :black
-highclip = :yellow
+colormap = cgrad(:thermal)[2:end-1]
+colorrange = extrema(T_ts[1])
+lowclip = cgrad(:thermal)[1]
+highclip = cgrad(:thermal)[end]
 #  Temperatures
-animate_2D_field(T_ts, "Θ (°C)", 10, 10; colormap, colorrange)
+animate_2D_field(T_ts, "Θ (°C)", 10, 10; colormap, colorrange, highclip, lowclip)
 ## Salinity
 @info "Animating salinity"
-colormap = :haline
-colorrange = extrema(S_ts)
-lowclip = :black
-highclip = :yellow
-animate_2D_field(S_ts, "S (gkg⁻¹)", 10, 10; colormap, colorrange,
-                                            highclip, lowclip)
+colormap = cgrad(:haline)[2:end-1]
+colorrange = extrema(S_ts[1])
+lowclip = cgrad(:haline)[1]
+highclip = cgrad(:haline)[end]
+animate_2D_field(S_ts, "S (gkg⁻¹)", 10, 10; colormap, colorrange, highclip, lowclip)
 ## Density (x-z)
 @info "Animating density"
 colormap = cgrad(:dense)[2:end-1]
-colorrange = extrema(σ₀_ts),
-highclip = cgrad(:dense)[end]
+colorrange = extrema(σ₀_ts[1])
 lowclip = cgrad(:dense)[1]
-animate_2D_field(σ₀_ts, "σ₀ (kgm⁻³)", 10, 10; colormap, colorrange,
-                                              highclip, lowclip)
+highclip = cgrad(:dense)[end]
+animate_2D_field(σ₀_ts, "σ₀ (kgm⁻³)", 10, 10; colormap, colorrange, highclip, lowclip)
--- data/analysis/read_DNSoutput.jl
+++ data/analysis/read_DNSoutput.jl
@@ -3,8 +3,8 @@ using DirectNumericalCabbelingShenanigans.OutputUtilities: compute_density
 
 ## Load in saved output
 sim_path = joinpath(SIMULATION_PATH, "stable.jld2")
-T_ts = FieldTimeSeries(sim_path, "T")
-S_ts = FieldTimeSeries(sim_path, "S")
+T_ts = FieldTimeSeries(sim_path, "T", backend = OnDisk())
+S_ts = FieldTimeSeries(sim_path, "S", backend = OnDisk())
 
 ## Compute a density `FieldTimeSeries`
 σ₀_ts = compute_density(S_ts, T_ts)
