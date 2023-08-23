e1dca720e5d1c51ff5823590bea1c15655d3661c
jbisits <jbisits@gmail.com>
2023-08-23 13:28:43 +1000

    Update stabletanhic_output.jl

--- data/analysis/stabletanhic_output.jl
+++ data/analysis/stabletanhic_output.jl
@@ -7,8 +7,11 @@ visualise_snapshot(σ₀_ts, "σ₀ (kgm⁻³)", 10, 10, 10; colormap = :dense)
 
 ## Animations (x-z)
 #  Temperatures
-animate_2D_field(T_ts, "Θ (°C)", 10, 10)
+animate_2D_field(T_ts, "Θ (°C)", 10, 10; colormap = cgrad(:thermal), colorrange = extrema(T_ts),
+                                         highclip = cgrad(:thermal)[end], lowclip = cgrad(:thermal)[1])
 ## Salinity
-animate_2D_field(T_ts, "S (gkg⁻¹)", 10, 10; colormap = :haline)
+animate_2D_field(T_ts, "S (gkg⁻¹)", 10, 10; colormap = cgrad(:haline), colorrange = extrema(S_ts),
+                                            highclip = cgrad(:haline)[end], lowclip = cgrad(:haline)[1])
 ## Density (x-z)
-animate_2D_field(σ₀_ts, "σ₀ (kgm⁻³)", 10, 10; colormap = :dense)
+animate_2D_field(σ₀_ts, "σ₀ (kgm⁻³)", 10, 10; colormap = cgrad(:dense), colorrange = extrema(σ₀_ts),
+                                              highclip = cgrad(:dense)[end], lowclip = cgrad(:dense)[1])
