11c17500638651241731a6fc28a84a4fab44745b
jbisits <jbisits@gmail.com>
2023-08-25 12:16:02 +1000

    field time series mean (for `OnDisk()`)

--- data/analysis/kolmogorov_scales.jl
+++ data/analysis/kolmogorov_scales.jl
@@ -1,2 +1,14 @@
 "Calculate the Kolmogorov length scale `η` from viscousity and average TKE dissapation."
 η(ν, ϵ) = (ν^3 / ϵ)^(1/4)
+
+function field_ts_mean(field_ts::FieldTimeSeries)
+
+    t = field_ts.times
+    field_data = field_ts[1].data
+    for i ∈ 2:length(t)
+        field_data .+= field_ts[i].data
+    end
+
+    return field_data ./ length(t)
+
+end
