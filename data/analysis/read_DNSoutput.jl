aa40d1c4c08eb95e285895874fd9886cf730a1cd
jbisits <jbisits@gmail.com>
2023-08-25 11:32:23 +1000

    Update read_DNSoutput.jl

--- data/analysis/read_DNSoutput.jl
+++ data/analysis/read_DNSoutput.jl
@@ -1,7 +1,7 @@
 using Oceananigans.Fields
 
 ## Load (lazily) in saved output
-sim_path = joinpath(SIMULATION_PATH, "isohaline_largeperturbation.jld2")
+sim_path = joinpath(SIMULATION_PATH, "isohaline_largeperturbation_10min.jld2")
 T_ts = FieldTimeSeries(sim_path, "T", backend = OnDisk())
 S_ts = FieldTimeSeries(sim_path, "S", backend = OnDisk())
 ϵ_ts = FieldTimeSeries(sim_path, "ϵ", backend = OnDisk())
