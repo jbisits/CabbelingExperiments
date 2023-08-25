ddd86f417577ea78e1d043a69e814f80311e17eb
jbisits <jbisits@gmail.com>
2023-08-25 11:31:13 +1000

    Read file

---
+++ data/analysis/kolmogorov_scales.jl
@@ -0,0 +1,2 @@
+"Calculate the Kolmogorov length scale `η` from viscousity and average TKE dissapation."
+η(ν, ϵ) = (ν^3 / ϵ)^(1/4)
--- data/analysis/read_DNSoutput.jl
+++ data/analysis/read_DNSoutput.jl
@@ -1,7 +1,8 @@
 using Oceananigans.Fields
 
 ## Load (lazily) in saved output
-sim_path = joinpath(SIMULATION_PATH, "stable.jld2")
+sim_path = joinpath(SIMULATION_PATH, "isohaline_largeperturbation.jld2")
 T_ts = FieldTimeSeries(sim_path, "T", backend = OnDisk())
 S_ts = FieldTimeSeries(sim_path, "S", backend = OnDisk())
+ϵ_ts = FieldTimeSeries(sim_path, "ϵ", backend = OnDisk())
 σ₀_ts = FieldTimeSeries(sim_path, "σ₀", backend = OnDisk())
