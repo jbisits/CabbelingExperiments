cf738ca769c049045d4d888606348d3695d46235
jbisits <jbisits@gmail.com>
2023-08-25 12:59:27 +1000

    Update kolmogorov_scales.jl

--- data/analysis/kolmogorov_scales.jl
+++ data/analysis/kolmogorov_scales.jl
@@ -1,6 +1,7 @@
 "Calculate the Kolmogorov length scale `η` from viscousity and average TKE dissapation."
 η(ν, ϵ) = (ν^3 / ϵ)^(1/4)
 
+"Return the mean from a `FieldTimeSeries` that is `OnDisk()`."
 function field_ts_timemean(field_ts::FieldTimeSeries)
 
     t = field_ts.times
@@ -10,5 +11,17 @@ function field_ts_timemean(field_ts::FieldTimeSeries)
     end
 
     return field_data ./ length(t)
+
+end
+"Find the minimum `η` from the output `ϵ` time series."
+function minimum_η(ϵ::FieldTimeSeries; ν = 1e-6)
+
+    t = field_ts.times
+    minimum_η_t = similar(t)
+    for i ∈ eachindex(t)
+        minimum_η_t[i] = minimum(η.(ν, ϵ[i]))
+    end
+
+    return (minimum_η_t)
 
 end
