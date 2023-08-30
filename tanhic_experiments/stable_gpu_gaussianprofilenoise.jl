a445b4468c143e30872f609a2192c8cdaa2405b9
jbisits <jbisits@gmail.com>
2023-08-31 07:26:32 +1000

    Update depth

--- tanhic_experiments/stable_gpu_gaussianprofilenoise.jl
+++ tanhic_experiments/stable_gpu_gaussianprofilenoise.jl
@@ -21,7 +21,7 @@ tracer_perturbation = SalinityGaussianProfile(INTERFACE_LOCATION, INTERFACE_LOCA
 # depth_idx = findfirst(z .> INTERFACE_LOCATION / 1.1)
 #initial_noise = RandomPerturbations(-0.34077380952380953, 0.001)
 #depth = find_depth(model, INTERFACE_LOCATION / 1.1)
-initial_noise = SalinityNoise(-0.34077380952380953, 0.001)
+initial_noise = SalinityNoise(-0.34047619048073924, 0.001)
 dns = TwoLayerDNS(model, profile_function, initial_conditions;
                   tracer_perturbation, initial_noise)
 set_two_layer_initial_conditions!(dns)
