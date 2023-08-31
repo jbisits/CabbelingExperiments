7376f94a50101b1c371496f5015655ddc7476fe2
jbisits <jbisits@gmail.com>
2023-08-31 14:48:34 +1000

    For checking indexing on gpu

---
+++ cpu_experiments/gpuindexcheck.jl
@@ -0,0 +1,11 @@
+using TwoLayerDirectNumericalShenanigans
+
+architecture = GPU()
+diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
+
+## Setup the model
+model = DNS(architecture, DOMAIN_EXTENT, HIGH_RESOLUTION, diffusivities;
+            reference_density = REFERENCE_DENSITY,
+            linear_eos = true)
+
+depth = find_depth(model, -0.5)
