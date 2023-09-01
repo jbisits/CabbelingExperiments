d10f9e2528e37f5097876f088936c3ef516c8a49
jbisits <jbisits@gmail.com>
2023-09-01 14:28:14 +1000

    Update animate_fields.jl

--- data/analysis/animate_fields.jl
+++ data/analysis/animate_fields.jl
@@ -1,20 +1,20 @@
 using CairoMakie, TwoLayerDirectNumericalShenanigans
 
-# ## Animations (x-z)
-# @info "Animating temperature"
-# colormap = cgrad(:thermal)[2:end-1]
-# colorrange = extrema(T_ts[1])
-# lowclip = cgrad(:thermal)[1]
-# highclip = cgrad(:thermal)[end]
-# #  Temperatures
-# animate_2D_field(T_ts, "Θ (°C)", 10, 10; colormap, colorrange, highclip, lowclip)
-# ## Salinity
-# @info "Animating salinity"
-# colormap = cgrad(:haline)[2:end-1]
-# colorrange = extrema(S_ts[1])
-# lowclip = cgrad(:haline)[1]
-# highclip = cgrad(:haline)[end]
-# animate_2D_field(S_ts, "S (gkg⁻¹)", 10, 10; colormap, colorrange, highclip, lowclip)
+## Animations (x-z)
+@info "Animating temperature"
+colormap = cgrad(:thermal)[2:end-1]
+colorrange = extrema(T_ts[1])
+lowclip = cgrad(:thermal)[1]
+highclip = cgrad(:thermal)[end]
+#  Temperatures
+animate_2D_field(T_ts, "Θ (°C)", 10, 10; colormap, colorrange, highclip, lowclip)
+## Salinity
+@info "Animating salinity"
+colormap = cgrad(:haline)[2:end-1]
+colorrange = extrema(S_ts[1])
+lowclip = cgrad(:haline)[1]
+highclip = cgrad(:haline)[end]
+animate_2D_field(S_ts, "S (gkg⁻¹)", 10, 10; colormap, colorrange, highclip, lowclip)
 ## Density (x-z)
 @info "Animating density"
 colormap = cgrad(:dense)[2:end-1]
