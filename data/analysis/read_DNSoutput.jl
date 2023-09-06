bd67b593bef4c4b18248db75e2c2a49de667adf3
jbisits <jbisits@gmail.com>
2023-09-06 15:30:30 +1000

    Update read_DNSoutput.jl

--- data/analysis/read_DNSoutput.jl
+++ data/analysis/read_DNSoutput.jl
@@ -4,4 +4,4 @@ using Oceananigans.Fields
 sim_path = joinpath(SIMULATION_PATH, "verystable_tanh_salinitygaussianprofile_mp_10min.jld2")
 T_ts = FieldTimeSeries(sim_path, "T", backend = OnDisk())
 S_ts = FieldTimeSeries(sim_path, "S", backend = OnDisk())
-σ₀_ts = FieldTimeSeries(sim_path, "σ₀", backend = OnDisk())
+# σ₀_ts = FieldTimeSeries(sim_path, "σ₀", backend = OnDisk())
