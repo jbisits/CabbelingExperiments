e2c73e299ec1b11be9dde8c27b3b89603bf43fed
jbisits <jbisits@gmail.com>
2023-09-06 13:08:57 +1000

    Create animate_raster.jl

---
+++ data/analysis/animate_raster.jl
@@ -0,0 +1,30 @@
+using CairoMakie, TwoLayerDirectNumericalShenanigans, Rasters, NCDatasets
+
+## Animations (x-z)
+@info "Reading into T into Raster"
+rs = Raster(stable_lp, name = :T)
+@info "Animating temperature"
+colormap = cgrad(:thermal)[2:end-1]
+colorrange = extrema(rs)
+lowclip = cgrad(:thermal)[1]
+highclip = cgrad(:thermal)[end]
+#  Temperatures
+animate_2D_field(rs, 10, 10; colormap, colorrange, highclip, lowclip)
+## Salinity
+@info "Reading into S into Raster"
+rs = Raster(stable_lp, name = :T)
+@info "Animating salinity"
+colormap = cgrad(:haline)[2:end-1]
+colorrange = extrema(rs)
+lowclip = cgrad(:haline)[1]
+highclip = cgrad(:haline)[end]
+animate_2D_field(rs, 10, 10; colormap, colorrange, highclip, lowclip)
+## Density (x-z)
+# @info "Reading into S into Raster"
+# rs = Raster(stable_lp, name = :σ)
+# @info "Animating density"
+# colormap = cgrad(:dense)[2:end-1]
+# colorrange = extrema(sr)
+# lowclip = cgrad(:dense)[1]
+# highclip = cgrad(:dense)[end]
+# animate_2D_field(rs, 10, 10; colormap, colorrange, highclip, lowclip)
