0e220bafc43460b727eaf083b13b13208bc9eb75
jbisits <jbisits@gmail.com>
2023-09-06 10:24:53 +1000

    Update gpuindexcheck.jl

--- cpu_experiments/gpuindexcheck.jl
+++ cpu_experiments/gpuindexcheck.jl
@@ -5,7 +5,6 @@ diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
 
 ## Setup the model
 model = DNS(architecture, DOMAIN_EXTENT, HIGH_RESOLUTION, diffusivities;
-            reference_density = REFERENCE_DENSITY,
-            linear_eos = true)
+            reference_density = REFERENCE_DENSITY)
 
 depth = find_depth(model, -0.5)
