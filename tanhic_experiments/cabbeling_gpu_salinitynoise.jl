04576b1c361cbab4f055c18127adf5a97e0d893e
jbisits <jbisits@gmail.com>
2023-09-11 13:39:53 +1000

    Update cabbeling_gpu_salinitynoise.jl
    Longer

--- tanhic_experiments/cabbeling_gpu_salinitynoise.jl
+++ tanhic_experiments/cabbeling_gpu_salinitynoise.jl
@@ -23,7 +23,7 @@ set_two_layer_initial_conditions!(dns)
 
 ## build the simulation
 Δt = 1e-5
-stop_time = 5 * 60
+stop_time = 10 * 60
 save_schedule = 5 # seconds
 simulation = DNS_simulation_setup(dns, Δt, stop_time, save_schedule)
 
