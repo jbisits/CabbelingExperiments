f1e88d1e1780ec44ab3cf2a5cd99574687b4eb01
jbisits <jbisits@gmail.com>
2023-09-12 12:15:23 +1000

    New experiments for `MidPoint` ic

---
+++ midpt_experiments/stable_gpu_salinitynoise.jl
@@ -0,0 +1,30 @@
+using TwoLayerDirectNumericalShenanigans
+
+architecture = GPU()
+diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
+
+## Setup the model
+model = DNS(architecture, DOMAIN_EXTENT, HIGH_RESOLUTION, diffusivities;
+            reference_density = REFERENCE_DENSITY)
+
+## set initial conditions
+T₀ᵘ = -1.5
+S₀ᵘ = 34.551
+stable = StableUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
+initial_conditions = TwoLayerInitialConditions(stable)
+depth = find_depth(model, INTERFACE_LOCATION)
+profile_function = MidPoint(depth)
+
+## Salinity noise
+initial_noise = SalinityNoise(depth, 0.001)
+dns = TwoLayerDNS(model, profile_function, initial_conditions; initial_noise)
+set_two_layer_initial_conditions!(dns)
+
+## build the simulation
+Δt = 1e-5
+stop_time = 10 * 60 # seconds (in simulation time)
+save_schedule = 5 # seconds
+simulation = DNS_simulation_setup(dns, Δt, stop_time, save_schedule)
+
+## Run the simulation
+run!(simulation)
--- notebooks/initial_density_profile.jl
+++ notebooks/initial_density_profile.jl
@@ -41,11 +41,13 @@ begin
 	## Setup the model
 	model = DNS(architecture, DOMAIN_EXTENT, HIGH_RESOLUTION, diffusivities;
 	            reference_density = REFERENCE_DENSITY)
+	model_mp = DNS(architecture, DOMAIN_EXTENT, HIGH_RESOLUTION, diffusivities;
+	            reference_density = REFERENCE_DENSITY)
 end
 
 # ╔═╡ a1101bd0-55e9-48d3-bcfd-736e1d961bfb
 md"""
-## Setting salinity and temperature
+## Setting salinity and temperature with `HyperbolicTangent`
 
 To determine the stable and unstable to cabbeling initial conditions I have been using my results from project one.
 Choosing the stable vs cabbeling initial the instability in the density profile can be seen.
@@ -129,6 +131,8 @@ let
 	S_star, Θ_star = 34.7, 0.5
 	S_s, Θ_s = S₀ᵘ.stable, T₀ᵘ
 	S_c = isequal(ulic, "cabbeling") ? S₀ᵘ.cabbeling : S₀ᵘ.cabbeling2
+	S_mid = 0.5*(S_c + S_star)
+	Θ_mid = 0.5*(Θ_star + Θ_s)
 	T_profile = interior(dns.model.tracers.T, 1, 1, :)
 	S_profile = interior(dns.model.tracers.S, 1, 1, :)
 	Θ_c = T₀ᵘ
@@ -158,8 +162,129 @@ let
 	lines!(ax, S_range, Θ_linear, color = :blue, linestyle = :dash)
 	scatterlines!(ax, S_profile, T_profile, color = :purple, label = "Initial profile")
 	scatter!(ax, [S_star], [Θ_star]; color = :red, label = "Lower layer")
+	#scatter!(ax, [S_mid], [Θ_mid])
 	axislegend(ax, position = :rb)
 	fig
+	#save("initialTSprofiles.png", fig)
+end
+
+# ╔═╡ 924b4be2-5ed1-4dc1-8f5e-95d5dcbea781
+md"""
+### Setting a midpoint transition between the upper and lower layers
+
+Can see below that we should solve the problem using a midpoint transition as in figure below.
+Clearly this midpoint will only work for *certain* cases but if you can specify where this transition takes pace or find the ``S`` and ``T`` values that give you the *last statically stable transition point* this should work.
+
+As we have a stable profile this should work fine but will have to just set one up and try.
+"""
+
+# ╔═╡ d08c96fc-054c-4c0a-836d-da71744f4366
+let
+	S_star, Θ_star = 34.7, 0.5
+	S_s, Θ_s = S₀ᵘ.stable, T₀ᵘ
+	S_c = isequal(ulic, "stable") ? S₀ᵘ.stable : isequal(ulic, "cabbeling") ? S₀ᵘ.cabbeling : S₀ᵘ.cabbeling2
+	S_mid = 0.5*(S_c + S_star)
+	Θ_mid = 0.5*(Θ_star + Θ_s)
+	Θ_c = T₀ᵘ
+	N = 2000
+	S_range, Θ_range = range(34.52, 34.72, length = N), range(-2, 1, length = N)
+	S_grid, Θ_grid = ones(N) .* S_range', ones(N)' .* Θ_range
+	ρ = gsw_rho.(S_grid, Θ_grid, 0)
+	ρ_star = gsw_rho(S_star, Θ_star, 0)
+	α_star = gsw_alpha(S_star, Θ_star, 0)
+	β_star = gsw_beta(S_star, Θ_star, 0)
+	ρ_s = gsw_rho(S_s, Θ_s, 0)
+	find_Θ = findfirst(Θ_range .> -1.5)
+	find_S = findfirst(ρ[find_Θ, :] .> ρ_star)
+	S_iso, Θ_iso = S_range[find_S], Θ_range[find_Θ]
+	gsw_rho(S_iso, Θ_iso, 0)
+
+	αₗ, βₗ = gsw_alpha(S_star, Θ_star, 0), gsw_beta(S_star, Θ_star, 0)
+	m = βₗ / αₗ
+	Θ_linear = @. Θ_star + m * (S_range - S_star)
+	fig = Figure(size = (500, 500), fontsize = 22)
+	ax = Axis(fig[1, 1];
+			  title = "Water masses in salinity-temperature space",
+			  xlabel = "Absolute salinity (gkg⁻¹)",
+			  ylabel = "Conservative temperature (°C)",
+			  limits = (extrema(S_range), extrema(Θ_range)))
+	contour!(ax, S_range, Θ_range, ρ'; levels = [ρ_star], color = :black, linewidth = 0.4, labelsize = 18)
+	lines!(ax, S_range, Θ_linear, color = :blue, linestyle = :dash)
+	scatterlines!(ax, [S_star, S_mid, S_c], [Θ_star, Θ_mid, Θ_c], color = :purple, label = "Initial profile")
+	scatter!(ax, [S_star], [Θ_star]; color = :red, label = "Lower layer")
+	#scatter!(ax, [S_mid], [Θ_mid])
+	axislegend(ax, position = :rb)
+	fig
+	#save("initialTSprofiles.png", fig)
+end
+
+# ╔═╡ a9b3ff4f-c546-494f-9c2d-31664a5aeccc
+let
+	S_star, Θ_star = 34.7, 0.5
+	S_s, Θ_s = S₀ᵘ.stable, T₀ᵘ
+	S_c = isequal(ulic, "stable") ? S₀ᵘ.stable : isequal(ulic, "cabbeling") ? S₀ᵘ.cabbeling : S₀ᵘ.cabbeling2
+	S_mid = 0.5*(S_c + S_star)
+	Θ_mid = 0.5*(Θ_star + Θ_s)
+	Δσ = gsw_rho(S_star, Θ_star, 0) - gsw_rho(S_mid, Θ_mid, 0)
+end
+
+# ╔═╡ 7d960e45-7257-4283-ac60-89aebaf04976
+md"""
+## Setting with `MidPoint`
+
+I have written an option to set a `Midpoint` transition at some depth (that can be specified) based on the notes above.
+
+It performs everything as it should so provided it does not blow up when the simulation starts (and this could be refined with more strategically placed nodes) I think it could be there.
+"""
+
+# ╔═╡ da31a2a7-f171-417e-a1c4-c1e79b497758
+begin
+	profile_function_mp = MidPoint(depth)
+	dns_mp = TwoLayerDNS(model_mp, profile_function_mp, initial_conditions; initial_noise)
+	set_two_layer_initial_conditions!(dns_mp)
+	visualise_initial_density(dns_mp, 1, 1, 0)
+end
+
+# ╔═╡ 92a878fa-ead4-4cc6-8422-771c2fc8d035
+let
+	S_star, Θ_star = 34.7, 0.5
+	S_s, Θ_s = S₀ᵘ.stable, T₀ᵘ
+	S_c = isequal(ulic, "cabbeling") ? S₀ᵘ.cabbeling : S₀ᵘ.cabbeling2
+	S_mid = 0.5*(S_c + S_star)
+	Θ_mid = 0.5*(Θ_star + Θ_s)
+	T_profile = interior(dns_mp.model.tracers.T, 1, 1, :)
+	S_profile = interior(dns_mp.model.tracers.S, 1, 1, :)
+	Θ_c = T₀ᵘ
+	N = 2000
+	S_range, Θ_range = range(34.52, 34.72, length = N), range(-2, 1, length = N)
+	S_grid, Θ_grid = ones(N) .* S_range', ones(N)' .* Θ_range
+	ρ = gsw_rho.(S_grid, Θ_grid, 0)
+	ρ_star = gsw_rho(S_star, Θ_star, 0)
+	α_star = gsw_alpha(S_star, Θ_star, 0)
+	β_star = gsw_beta(S_star, Θ_star, 0)
+	ρ_s = gsw_rho(S_s, Θ_s, 0)
+	find_Θ = findfirst(Θ_range .> -1.5)
+	find_S = findfirst(ρ[find_Θ, :] .> ρ_star)
+	S_iso, Θ_iso = S_range[find_S], Θ_range[find_Θ]
+	gsw_rho(S_iso, Θ_iso, 0)
+
+	αₗ, βₗ = gsw_alpha(S_star, Θ_star, 0), gsw_beta(S_star, Θ_star, 0)
+	m = βₗ / αₗ
+	Θ_linear = @. Θ_star + m * (S_range - S_star)
+	fig = Figure(size = (500, 500), fontsize = 22)
+	ax = Axis(fig[1, 1];
+			  title = "Water masses in salinity-temperature space",
+			  xlabel = "Absolute salinity (gkg⁻¹)",
+			  ylabel = "Conservative temperature (°C)",
+			  limits = (extrema(S_range), extrema(Θ_range)))
+	contour!(ax, S_range, Θ_range, ρ'; levels = [ρ_star], color = :black, linewidth = 0.4, labelsize = 18)
+	lines!(ax, S_range, Θ_linear, color = :blue, linestyle = :dash)
+	scatterlines!(ax, S_profile, T_profile, color = :purple, label = "Initial profile")
+	scatter!(ax, [S_star], [Θ_star]; color = :red, label = "Lower layer")
+	#scatter!(ax, [S_mid], [Θ_mid])
+	axislegend(ax, position = :rb)
+	fig
+	#save("initialTSprofiles.png", fig)
 end
 
 # ╔═╡ 3e7fedfd-e0bd-4358-aa46-ce68fd90c015
@@ -200,6 +325,12 @@ TableOfContents(title = "Initial density profile")
 # ╟─796798a1-4700-4d1c-9afd-6053a77c7172
 # ╟─b9910098-4ec6-4e2d-91a4-c8538a91a05c
 # ╟─3d801dd0-c1d7-4c6f-9cd1-61cbbbcfdeeb
+# ╟─924b4be2-5ed1-4dc1-8f5e-95d5dcbea781
+# ╟─d08c96fc-054c-4c0a-836d-da71744f4366
+# ╟─a9b3ff4f-c546-494f-9c2d-31664a5aeccc
+# ╟─7d960e45-7257-4283-ac60-89aebaf04976
+# ╟─da31a2a7-f171-417e-a1c4-c1e79b497758
+# ╟─92a878fa-ead4-4cc6-8422-771c2fc8d035
 # ╟─3e7fedfd-e0bd-4358-aa46-ce68fd90c015
 # ╟─96c44a1f-96d4-4844-a363-f47752b9c014
 # ╟─9c88315a-11e9-421a-9ec2-f6b1398a5bd3
