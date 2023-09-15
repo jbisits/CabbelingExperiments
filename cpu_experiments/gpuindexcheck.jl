2d9710e3cf1ce314800f23b7968612a6bb92503b
jbisits <jbisits@gmail.com>
2023-09-15 20:59:39 +1000

    Update gpuindexcheck.jl

--- cpu_experiments/gpuindexcheck.jl
+++ cpu_experiments/gpuindexcheck.jl
@@ -4,8 +4,26 @@ architecture = GPU()
 diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
 
 ## Setup the model
+@info "Setting up model"
 model = DNS(architecture, DOMAIN_EXTENT, HIGH_RESOLUTION, diffusivities;
             reference_density = REFERENCE_DENSITY)
 
-depth = find_depth(model, INTERFACE_LOCATION / 1.1)
+# depth = find_depth(model, INTERFACE_LOCATION / 1.1)
+# depths = find_depth(model, [INTERFACE_LOCATION + 0.02, INTERFACE_LOCATION - 0.02])
+# scales = similar(depths)
+@info "Building DNS"
+T₀ᵘ = -1.5
+S₀ᵘ = 34.551
+stable = StableUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
+initial_conditions = TwoLayerInitialConditions(stable)
+profile_function = HyperbolicTangent(INTERFACE_LOCATION, 3500.0)
+
 depths = find_depth(model, [INTERFACE_LOCATION + 0.02, INTERFACE_LOCATION - 0.02])
+scales = similar(depths)
+fill!(scales, 1e-4)
+initial_noise = SalinityNoise(Array(depths), Array(scales))
+
+dns = TwoLayerDNS(model, profile_function, initial_conditions; initial_noise)
+
+@info "Setting two layer initial conditions"
+set_two_layer_initial_conditions!(dns)
