f8ea7ee7a9712f1f314f8ef91ab1707ca66b2fb7
jbisits <jbisits@gmail.com>
2023-09-19 16:50:17 +1000

    Experiments to test stability of system
    as resolution increases and different diffusivity is used

---
+++ test_experiments/different_diffusivity.jl
@@ -0,0 +1,37 @@
+using TwoLayerDirectNumericalShenanigans
+
+architecture = GPU()
+
+## Setup the model
+@info "Model setup"
+model = DNS(architecture, DOMAIN_EXTENT, HIGH_RESOLUTION, SO_DIFFUSIVITIES;
+            reference_density = REFERENCE_DENSITY)
+
+## set initial conditions
+@info "Setting initial conditions"
+T₀ᵘ = -1.5
+S₀ᵘ = 34.58
+cabbeling = CabbelingUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
+initial_conditions = TwoLayerInitialConditions(cabbeling)
+depth = find_depth(model, INTERFACE_LOCATION)
+profile_function = StepChange(depth)
+
+## Salinity noise
+depths = find_depth(model, [INTERFACE_LOCATION + 0.02, INTERFACE_LOCATION - 0.02])
+scales = similar(depths)
+fill!(scales, 2e-4)
+initial_noise = SalinityNoise(depths, scales)
+@info "Building DNS"
+dns = TwoLayerDNS(model, profile_function, initial_conditions; initial_noise)
+
+@info "Setting two layer initial conditions"
+set_two_layer_initial_conditions!(dns)
+
+## build the simulation
+Δt = 1e-4
+stop_time = 10 * 60
+save_schedule = 5 # seconds
+simulation = DNS_simulation_setup(dns, Δt, stop_time, save_schedule)
+
+## Run the simulation
+run!(simulation)
