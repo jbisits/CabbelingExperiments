2d04c8c4281460ae98c19799ccb027b40344e864
jbisits <jbisits@gmail.com>
2023-10-05 10:00:08 +1100

    Update gpu_kfsim.jl

--- test_experiments/gpu_kfsim.jl
+++ test_experiments/gpu_kfsim.jl
@@ -20,55 +20,61 @@ function DNS_simulation_setup_test(dns::TwoLayerDNS, Δt::Number,
     # custom saved output
 
     # Density
-    σ = DensityField(model, density_reference_pressure)
+    parameters = (pᵣ = density_reference_pressure,)
+    σ = DensityField(model, parameters)
 
     # Inferred vertical diffusivity
-    # σ_anomaly_interpolated = TLDNS.InterpolatedDensityAnomaly(model, density_reference_pressure)
-    # w = model.velocities.w
-    # κᵥ = Integral((-w * σ_anomaly_interpolated) / σ)
+    b_anomaly = BuoyancyField(model)
+    b_zgrad = Field(∂b∂z(model))
+
+    wᶜᶜᶜ_anomaly = Field(wᶜᶜᶜ(dns.model))
+    κᵥ = Integral((-wᶜᶜᶜ_anomaly * b_anomaly) / b_zgrad)
 
     # Minimum in space Kolmogorov length scale
-    #ϵ = KineticEnergyDissipationRate(model)
-    #η_space(model) = minimum(model.closure.ν ./ ϵ)
+    ϵ = KineticEnergyDissipationRate(model)
+    ∫ϵ = Integral(ϵ)
+    η_space(model) = model.closure.ν / maximum(ϵ)
 
     # Dimensions and attributes for custom saved output
-    dims = Dict("σ" => ("xC", "xC", "zC"))
+    dims = Dict("η_space" => (), "σ" => ("xC", "xC", "zC"), "κᵥ" => (), "∫ϵ" => ())
     oa = Dict(
-    "σ" => Dict("longname" => "Seawater potential density calculated using TEOS-10 at $(density_reference_pressure)dbar",
-    "units" => "kgm⁻³"))
-    #"η_space" => Dict("longname" => "Minimum (in space) Kolmogorov length"),
-    #"κᵥ" => Dict("longname" => "Inferred vertical diffusivity",
-    #"units" => "m²s⁻¹"))
+        "σ" => Dict("longname" => "Seawater potential density calculated using TEOS-10 at $(density_reference_pressure)dbar",
+                    "units" => "kgm⁻³"),
+        "η_space" => Dict("longname" => "Minimum (in space) Kolmogorov length"),
+        "κᵥ" => Dict("longname" => "Inferred vertical diffusivity",
+                     "units" => "m²s⁻¹"),
+        "∫ϵ" => Dict("longname" => "Volume integrated turbulent kintetic energy dissipation")
+        )
 
     # outputs to be saved during the simulation
-    outputs = Dict("S" => S, "T" => T, "σ" => σ)
+    outputs = Dict("S" => S, "T" => T, "η_space" => η_space, "σ" => σ, "κᵥ" => κᵥ, "∫ϵ" => ∫ϵ)
     if save_velocities
     u, v = model.velocities.u, model.velocities.v
     velocities = Dict("u" => u, "v" => v, "w" => w)
     merge!(outputs, velocities)
-end
+    end
 
-filename = TLDNS.form_filename(dns, stop_time, output_writer)
-simulation.output_writers[:outputs] = output_writer == :netcdf ?
-NetCDFOutputWriter(model, outputs,
-            filename = filename,
-            schedule = TimeInterval(save_schedule),
-            overwrite_existing = true,
-            dimensions = dims,
-            output_attributes = oa
-            ) :
-JLD2OutputWriter(model, outputs,
-            filename = filename,
-            schedule = TimeInterval(save_schedule),
-            overwrite_existing = true)
+    filename = TLDNS.form_filename(dns, stop_time, output_writer)
+    simulation.output_writers[:outputs] = output_writer == :netcdf ?
+    NetCDFOutputWriter(model, outputs,
+                filename = filename,
+                schedule = TimeInterval(save_schedule),
+                overwrite_existing = true,
+                dimensions = dims,
+                output_attributes = oa
+                ) :
+    JLD2OutputWriter(model, outputs,
+                filename = filename,
+                schedule = TimeInterval(save_schedule),
+                overwrite_existing = true)
 
-TLDNS.non_dimensional_numbers!(simulation, dns)
-TLDNS.predicted_maximum_density!(simulation, dns)
+    TLDNS.non_dimensional_numbers!(simulation, dns)
+    TLDNS.predicted_maximum_density!(simulation, dns)
 
-# progress reporting
-simulation.callbacks[:progress] = Callback(simulation_progress, IterationInterval(100))
+    # progress reporting
+    simulation.callbacks[:progress] = Callback(simulation_progress, IterationInterval(100))
 
-return simulation
+    return simulation
 
 end
 simulation_progress(sim) = @printf("i: % 6d, sim time: % 1.3f, wall time: % 10s, Δt: % 1.4f, advective CFL: %.2e, diffusive CFL: %.2e\n",
