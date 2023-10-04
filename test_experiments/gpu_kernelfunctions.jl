283d2b424820f3f7886b0af231f5159f4b350215
jbisits <jbisits@gmail.com>
2023-10-05 09:39:18 +1100

    Model into gpu_kernelfunctions.jl for testing onGPU

--- test_experiments/gpu_kernelfunctions.jl
+++ test_experiments/gpu_kernelfunctions.jl
@@ -3,8 +3,37 @@ using Oceananigans: BuoyancyModels.ρ′, BuoyancyModels.get_temperature_and_sal
 using Oceananigans: BuoyancyModels.∂z_b
 using Oceananigans: Operators.ℑzᵃᵃᶠ
 import Oceananigans.BuoyancyModels.ρ′
-Oceananigans.BuoyancyModels.ρ′(i, j, k, grid, eos, θ, sᴬ, pᵣ) =
-    ρ′(θ_and_sᴬ(i, j, k, θ, sᴬ)..., pᵣ, eos)
+Oceananigans.BuoyancyModels.ρ′(i, j, k, grid, eos, θ, sᴬ, pᵣ) = ρ′(θ_and_sᴬ(i, j, k, θ, sᴬ)..., pᵣ, eos)
+
+## A test model on the GPU
+architecture = GPU()
+diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
+DNS_resolution = (Nx = 10, Ny = 10, Nz = 100)
+
+## Setup the model
+@info "Model setup"
+model = DNS(architecture, DOMAIN_EXTENT, DNS_resolution, diffusivities;
+            reference_density = REFERENCE_DENSITY, zgrid_stretching = false)
+
+## set initial conditions
+@info "Setting initial conditions"
+T₀ᵘ = -1.5
+S₀ᵘ = 34.568
+cabbeling = CabbelingUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
+initial_conditions = TwoLayerInitialConditions(cabbeling)
+depth = find_depth(model, INTERFACE_LOCATION)
+profile_function = StepChange(depth)
+
+## Salinity noise
+depths = find_depth(model, [INTERFACE_LOCATION + 0.02, INTERFACE_LOCATION - 0.02])
+scales = similar(depths)
+fill!(scales, 2e-4)
+initial_noise = SalinityNoise(depths, scales)
+@info "Building DNS"
+dns = TwoLayerDNS(model, profile_function, initial_conditions; initial_noise)
+
+@info "Setting two layer initial conditions"
+set_two_layer_initial_conditions!(dns)
 
 ## Works on GPU.
 # @inline function densityᶜᶜᶜ(i, j, k, grid, b::SeawaterBuoyancy, C)
--- test_experiments/gpu_kfsim.jl
+++ test_experiments/gpu_kfsim.jl
@@ -1,51 +1,51 @@
 function DNS_simulation_setup_test(dns::TwoLayerDNS, Δt::Number,
-    stop_time::Number, save_schedule::Number,
-    output_writer::Symbol=:netcdf;
-    cfl = 0.75,
-    diffusive_cfl = 0.75,
-    max_change = 1.2,
-    max_Δt = 1e-1,
-    density_reference_pressure = 0,
-    save_velocities = false)
+        stop_time::Number, save_schedule::Number,
+        output_writer::Symbol=:netcdf;
+        cfl = 0.75,
+        diffusive_cfl = 0.75,
+        max_change = 1.2,
+        max_Δt = 1e-1,
+        density_reference_pressure = 0,
+        save_velocities = false)
 
-model = dns.model
-simulation = Simulation(model; Δt, stop_time)
+    model = dns.model
+    simulation = Simulation(model; Δt, stop_time)
 
-# time step adjustments
-wizard = TimeStepWizard(; cfl, diffusive_cfl, max_change, max_Δt)
-simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
+    # time step adjustments
+    wizard = TimeStepWizard(; cfl, diffusive_cfl, max_change, max_Δt)
+    simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(10))
 
-# model tracers
-S, T = model.tracers.S, model.tracers.T
-# custom saved output
+    # model tracers
+    S, T = model.tracers.S, model.tracers.T
+    # custom saved output
 
-# Density
-σ = DensityField(model, density_reference_pressure)
+    # Density
+    σ = DensityField(model, density_reference_pressure)
 
-# Inferred vertical diffusivity
-# σ_anomaly_interpolated = TLDNS.InterpolatedDensityAnomaly(model, density_reference_pressure)
-# w = model.velocities.w
-# κᵥ = Integral((-w * σ_anomaly_interpolated) / σ)
+    # Inferred vertical diffusivity
+    # σ_anomaly_interpolated = TLDNS.InterpolatedDensityAnomaly(model, density_reference_pressure)
+    # w = model.velocities.w
+    # κᵥ = Integral((-w * σ_anomaly_interpolated) / σ)
 
-# Minimum in space Kolmogorov length scale
-#ϵ = KineticEnergyDissipationRate(model)
-#η_space(model) = minimum(model.closure.ν ./ ϵ)
+    # Minimum in space Kolmogorov length scale
+    #ϵ = KineticEnergyDissipationRate(model)
+    #η_space(model) = minimum(model.closure.ν ./ ϵ)
 
-# Dimensions and attributes for custom saved output
-dims = Dict("σ" => ("xC", "xC", "zC"))
-oa = Dict(
-"σ" => Dict("longname" => "Seawater potential density calculated using TEOS-10 at $(density_reference_pressure)dbar",
-"units" => "kgm⁻³"))
-#"η_space" => Dict("longname" => "Minimum (in space) Kolmogorov length"),
-#"κᵥ" => Dict("longname" => "Inferred vertical diffusivity",
-#"units" => "m²s⁻¹"))
+    # Dimensions and attributes for custom saved output
+    dims = Dict("σ" => ("xC", "xC", "zC"))
+    oa = Dict(
+    "σ" => Dict("longname" => "Seawater potential density calculated using TEOS-10 at $(density_reference_pressure)dbar",
+    "units" => "kgm⁻³"))
+    #"η_space" => Dict("longname" => "Minimum (in space) Kolmogorov length"),
+    #"κᵥ" => Dict("longname" => "Inferred vertical diffusivity",
+    #"units" => "m²s⁻¹"))
 
-# outputs to be saved during the simulation
-outputs = Dict("S" => S, "T" => T, "σ" => σ)
-if save_velocities
-u, v = model.velocities.u, model.velocities.v
-velocities = Dict("u" => u, "v" => v, "w" => w)
-merge!(outputs, velocities)
+    # outputs to be saved during the simulation
+    outputs = Dict("S" => S, "T" => T, "σ" => σ)
+    if save_velocities
+    u, v = model.velocities.u, model.velocities.v
+    velocities = Dict("u" => u, "v" => v, "w" => w)
+    merge!(outputs, velocities)
 end
 
 filename = TLDNS.form_filename(dns, stop_time, output_writer)
@@ -75,35 +75,6 @@ simulation_progress(sim) = @printf("i: % 6d, sim time: % 1.3f, wall time: % 10s,
             iteration(sim), time(sim), prettytime(sim.run_wall_time),
             sim.Δt, AdvectiveCFL(sim.Δt)(sim.model),
             DiffusiveCFL(sim.Δt)(sim.model))
-## A test model on the GPU
-architecture = GPU()
-diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
-DNS_resolution = (Nx = 10, Ny = 10, Nz = 100)
-
-## Setup the model
-@info "Model setup"
-model = DNS(architecture, DOMAIN_EXTENT, DNS_resolution, diffusivities;
-            reference_density = REFERENCE_DENSITY, zgrid_stretching = false)
-
-## set initial conditions
-@info "Setting initial conditions"
-T₀ᵘ = -1.5
-S₀ᵘ = 34.568
-cabbeling = CabbelingUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
-initial_conditions = TwoLayerInitialConditions(cabbeling)
-depth = find_depth(model, INTERFACE_LOCATION)
-profile_function = StepChange(depth)
-
-## Salinity noise
-depths = find_depth(model, [INTERFACE_LOCATION + 0.02, INTERFACE_LOCATION - 0.02])
-scales = similar(depths)
-fill!(scales, 2e-4)
-initial_noise = SalinityNoise(depths, scales)
-@info "Building DNS"
-dns = TwoLayerDNS(model, profile_function, initial_conditions; initial_noise)
-
-@info "Setting two layer initial conditions"
-set_two_layer_initial_conditions!(dns)
 
 # ## build the simulation
 # Δt = 1e-4
