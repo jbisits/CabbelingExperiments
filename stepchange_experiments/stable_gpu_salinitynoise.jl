022cf87a7e685b4b66e945e39d63906bc9752fde
jbisits <jbisits@gmail.com>
2023-10-16 11:32:25 +1100

    More cleaning

--- stepchange_experiments/stable_gpu_salinitynoise.jl
+++ stepchange_experiments/stable_gpu_salinitynoise.jl
@@ -3,9 +3,9 @@ using TwoLayerDirectNumericalShenanigans
 architecture = GPU()
 diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
 
-## Setup the model
+## Setup the dns_model
 @info "Model setup"
-model = DNS(architecture, DOMAIN_EXTENT, HIGH_RESOLUTION, diffusivities;
+dns_model = DNS(architecture, DOMAIN_EXTENT, HIGH_RESOLUTION, diffusivities;
             reference_density = REFERENCE_DENSITY)
 
 ## set initial conditions
@@ -14,16 +14,16 @@ T₀ᵘ = -1.5
 S₀ᵘ = 34.551
 stable = StableUpperLayerInitialConditions(S₀ᵘ, T₀ᵘ)
 initial_conditions = TwoLayerInitialConditions(stable)
-depth = find_depth(model, INTERFACE_LOCATION)
+depth = find_depth(dns_model, INTERFACE_LOCATION)
 profile_function = StepChange(depth)
 
 ## Salinity noise
-depths = find_depth(model, [INTERFACE_LOCATION + 0.02, INTERFACE_LOCATION - 0.02])
+depths = find_depth(dns_model, [INTERFACE_LOCATION + 0.02, INTERFACE_LOCATION - 0.02])
 scales = similar(depths)
 fill!(scales, 2e-4)
 initial_noise = SalinityNoise(depths, scales)
 @info "Building DNS"
-dns = TwoLayerDNS(model, profile_function, initial_conditions; initial_noise)
+dns = TwoLayerDNS(dns_model, profile_function, initial_conditions; initial_noise)
 
 @info "Setting two layer initial conditions"
 set_two_layer_initial_conditions!(dns)
