5a20d3434501b8cd5f6b1922fec5b0a7d2bc3d9f
jbisits <jbisits@gmail.com>
2023-11-03 14:26:40 +1100

    Back to old experiment setup

--- cabbeling_gpu_salinitynoise.jl
+++ cabbeling_gpu_salinitynoise.jl
@@ -3,13 +3,12 @@ using TwoLayerDirectNumericalShenanigans
 restart = true
 
 architecture = GPU()
-resolution = (Nx = 32, Ny = 32, Nz = 320)
-diffusivities = (ν = 1e-5, κ = (S = 1e-6, T = 1e-6))
+diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
 eos = TEOS10EquationOfState(reference_density = REFERENCE_DENSITY)
 
 ## Setup the dns_model
 @info "Model setup"
-dns_model = DNSModel(architecture, DOMAIN_EXTENT, resolution, diffusivities, eos)
+dns_model = DNSModel(architecture, DOMAIN_EXTENT, HIGH_RESOLUTION, diffusivities, eos)
 
 ## set initial conditions
 @info "Setting initial conditions in upper layer"
@@ -36,7 +35,7 @@ set_two_layer_initial_conditions!(tldns)
 max_Δt = 0.1
 stop_time = 4 * 60 * 60 # seconds
 save_schedule = 60  # seconds
-checkpointer_time_interval = 5 * 60 # seconds
+checkpointer_time_interval = 30 * 60 # seconds
 output_path = joinpath(@__DIR__, "outputs_equaldiffusion/")
 @info "Setting up simulation"
 simulation = TLDNS_simulation_setup(tldns, Δt, stop_time, save_schedule, TLDNS.save_computed_output!;
--- stable_gpu_salinitynoise.jl
+++ stable_gpu_salinitynoise.jl
@@ -3,7 +3,7 @@ using TwoLayerDirectNumericalShenanigans
 restart = true
 
 architecture = GPU()
-diffusivities = (ν = 5e-6, κ = (S = 5e-9, T = 5e-7))
+diffusivities = (ν = 1e-6, κ = (S = 1e-7, T = 1e-7))
 eos = TEOS10EquationOfState(reference_density = REFERENCE_DENSITY)
 
 ## Setup the dns_model
@@ -35,7 +35,7 @@ set_two_layer_initial_conditions!(tldns)
 max_Δt = 0.075
 stop_time = 20 * 60 # seconds
 save_schedule = 30  # seconds
-checkpointer_time_interval = 5 * 60 # seconds
+checkpointer_time_interval = 30 * 60 # seconds
 output_path = joinpath(@__DIR__, "outputs_doublediffusion/")
 @info "Setting up simulation"
 simulation = TLDNS_simulation_setup(tldns, Δt, stop_time, save_schedule, TLDNS.save_computed_output!;
