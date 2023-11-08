3f0fb9a76408f00cf5d254e2dbc6f007b661f95a
jbisits <jbisits@gmail.com>
2023-11-09 09:42:15 +1100

    Update analysis_script.jl

--- data/analysis/analysis_script.jl
+++ data/analysis/analysis_script.jl
@@ -5,16 +5,16 @@ computed_output = joinpath(@__DIR__, "computed_output.nc")
 
 # Animate tracers
 @info "Animating tracers"
-animate_tracers(tracers)
+TLDNS.animate_tracers(tracers)
 
 # Animate density
 @info "Animating density"
-animate_density(computed_output)
+TLDNS.animate_density(computed_output)
 
 # Scalar diagnostics
 @info "Scalar diagnostics"
-plot_scalar_diagnostics(computed_output)
+TLDNS.plot_scalar_diagnostics(computed_output)
 
 # Hovmoller of inferred vertical diffusivity
 @info "Hovmoller of horizontally integrated inferred vertical diffusivity"
-hovmoller(computed_output, "∫ₐκᵥ", unit = "m²s⁻¹")
+TLDNS.hovmoller(computed_output, "∫ₐκᵥ", unit = "m²s⁻¹")
