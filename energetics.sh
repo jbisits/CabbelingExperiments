b54d850cce3a31e99989c82e0100b51288559c29
jbisits <jbisits@gmail.com>
2023-11-27 19:11:29 +1100

    Update energetics.sh

--- energetics.sh
+++ energetics.sh
@@ -3,12 +3,12 @@
 #PBS -P e14
 #PBS -l ncpus=1
 #PBS -l mem=96GB
-#PBS -l walltime=02:00:00
+#PBS -l walltime=03:00:00
 #PBS -l storage=gdata/e14+scratch/e14
 #PBS -l wd
 #PBS -M z5131023@unsw.edu.au
 
-cd /g/data/e14/jb2381/CabbelingExperiments/outputs_equaldiffusion/isothermal_stepchange_nothing_120min/
+cd /g/data/e14/jb2381/CabbelingExperiments/outputs_equaldiffusion/isothermal_stepchange_nothing_660min/
 
 # Julia
 export JULIA_DEPOT_PATH="/home/561/jb2381/.julia"
@@ -16,4 +16,4 @@ export JULIA_NUM_THREADS=auto
 module load julia
 
 # Run the experiment
-julia --project energetics.jl > $PBS_JOBID.log
+julia --project energetics_animations.jl > $PBS_JOBID.log
