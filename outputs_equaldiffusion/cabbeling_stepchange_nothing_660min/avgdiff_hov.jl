a2a1de3cf275ce4b6e778a5adf5e92e33b327edb
jbisits <jbisits@gmail.com>
2024-02-26 19:06:03 +1100

    Update avgdiff_hov.jl

--- outputs_equaldiffusion/cabbeling_stepchange_nothing_660min/avgdiff_hov.jl
+++ outputs_equaldiffusion/cabbeling_stepchange_nothing_660min/avgdiff_hov.jl
@@ -4,15 +4,15 @@
 using NCDatasets, CairoMakie, StatsBase
 
 ## Temperatre
-T_diff = "T_effective_diffusivity.nc"
+# T_diff = "T_effective_diffusivity.nc"
 
-dsT = NCDataset(T_diff, "a")
+# dsT = NCDataset(T_diff, "a")
 
-diffusivity = dsT[:κ_effectiveT][:, :]
-replace!(diffusivity, Inf => NaN)
-replace!(diffusivity, -Inf => NaN)
-t = dsT[:time_derivative][:]
-ez = dsT[:equivalent_z][:]
+# diffusivity = dsT[:κ_effectiveT][:, :]
+# replace!(diffusivity, Inf => NaN)
+# replace!(diffusivity, -Inf => NaN)
+# t = dsT[:time_derivative][:]
+# ez = dsT[:equivalent_z][:]
 
 # fig = Figure(size = (500, 500))
 # axT = Axis(fig[1, 1], xlabel = "time (s)", ylabel = "Equivalent z (m)",
@@ -23,54 +23,54 @@ ez = dsT[:equivalent_z][:]
 # @info "Saving figure"
 # save("Tdiffusivity_hov.png", fig)
 
-mean_diff = "κ_effectiveT_mean"
+# mean_diff = "κ_effectiveT_mean"
 
-if mean_diff ∉ keys(dsT)
+# if mean_diff ∉ keys(dsT)
 
-    @info "Computing mean"
-    defVar(dsT, "κ_effectiveT_mean", Float64, tuple("time_derivative"),
-            attrib = Dict("long_name" => "Vertically averaged effective diffusivity"))
-
-    for (i, c) ∈ enumerate(eachcol(diffusivity))
-
-        dsT[mean_diff][i] = mean(c[.!isnan.(c)])
-
-    end
-
-end
-
-close(dsT)
-
-## Salintiy
-# S_diff = "S_effective_diffusivity.nc"
-
-# fig = Figure(size = (500, 500))
-# axS = Axis(fig[1, 1], xlabel = "time (s)", ylabel = "Equivalent z (m)",
-#            title = "Effective Salinity diffusivity")
-
-# dsS = NCDataset(S_diff, "a")
-
-# diffusivity = dsS[:κ_effectiveS][:, :]
-# replace!(diffusivity, Inf => NaN)
-# replace!(diffusivity, -Inf => NaN)
-# hm = heatmap!(axS, ez, t, log10.(diffusivity), colormap = :balance)
-# Colorbar(fig[1, 2], hm, label = "Diffusivity (m²s⁻¹)")
-
-# save("Sdiffusivity_hov.png", fig)
-
-# mean_diff = "κ_effectiveS_mean"
-
-# if meand_diff ∉ keys(dsS)
-
-#     defVar(dsS, "κ_effectiveS_mean", Float64, tuple("time_derivative"),
+#     @info "Computing mean"
+#     defVar(dsT, "κ_effectiveT_mean", Float64, tuple("time_derivative"),
 #             attrib = Dict("long_name" => "Vertically averaged effective diffusivity"))
 
 #     for (i, c) ∈ enumerate(eachcol(diffusivity))
 
-#         dsS[mean_diff][i] = mean(c[.!isnan.(c)])
+#         dsT[mean_diff][i] = mean(c[.!isnan.(c)])
 
 #     end
 
 # end
 
-# close(dsS)
+# close(dsT)
+
+## Salintiy
+S_diff = "S_effective_diffusivity.nc"
+dsS = NCDataset(S_diff, "a")
+
+diffusivity = dsS[:κ_effectiveS][:, :]
+replace!(diffusivity, Inf => NaN)
+replace!(diffusivity, -Inf => NaN)
+
+fig = Figure(size = (500, 500))
+axS = Axis(fig[1, 1], xlabel = "time (s)", ylabel = "Equivalent z (m)",
+           title = "Effective Salinity diffusivity")
+
+hm = heatmap!(axS, ez, t, diffusivity, colormap = :balance)
+Colorbar(fig[1, 2], hm, label = "Diffusivity (m²s⁻¹)")
+
+save("Sdiffusivity_hov.png", fig)
+
+mean_diff = "κ_effectiveS_mean"
+
+if mean_diff ∉ keys(dsS)
+
+    defVar(dsS, "κ_effectiveS_mean", Float64, tuple("time_derivative"),
+            attrib = Dict("long_name" => "Vertically averaged effective diffusivity"))
+
+    for (i, c) ∈ enumerate(eachcol(diffusivity))
+
+        dsS[mean_diff][i] = mean(c[.!isnan.(c)])
+
+    end
+
+end
+
+close(dsS)
