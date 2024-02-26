ad988b013d11de3f1f534c47b21e829843d62df1
jbisits <jbisits@gmail.com>
2024-02-27 09:38:33 +1100

    Isothermal case

--- avgdiff_hov.sh
+++ avgdiff_hov.sh
@@ -2,13 +2,13 @@
 #PBS -q normalbw
 #PBS -P e14
 #PBS -l ncpus=1
-#PBS -l mem=196GB
+#PBS -l mem=128GB
 #PBS -l walltime=04:00:00
 #PBS -l storage=gdata/e14+scratch/e14
 #PBS -l wd
 #PBS -M z5131023@unsw.edu.au
 
-cd /g/data/e14/jb2381/CabbelingExperiments/outputs_equaldiffusion/cabbeling_stepchange_nothing_660min/
+cd /g/data/e14/jb2381/CabbelingExperiments/outputs_equaldiffusion/isothermal_stepchange_nothing_660min/
 
 # Julia
 export JULIA_DEPOT_PATH="/home/561/jb2381/.julia"
