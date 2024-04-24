e4b0b8171f8d18975d0ed95d7e49d1c381de971d
jbisits <jbisits@gmail.com>
2024-04-24 19:53:10 +1000

    Correct setup

--- 1DModel/run_onedmodel.jl
+++ 1DModel/run_onedmodel.jl
@@ -1,4 +1,4 @@
 include("onedmodel.jl")
 using .OneDModel
 
-run_OneDModel(:cabbeling, background_κz = 1e-2, convective_κz = 1.0, ν = 0.0, salinity_noise = true)
+run_OneDModel(:isothermal, background_κz = 1e-2, Tᵤ = 0.5, ν = 0.0, salinity_noise = true)
