3842ab6cb4635bc6d3e615bc920de6f49354f139
jbisits <jbisits@gmail.com>
2024-04-24 16:57:24 +1000

    Correct

--- 1DModel/run_onedmodel.jl
+++ 1DModel/run_onedmodel.jl
@@ -1,4 +1,4 @@
 include("onedmodel.jl")
 using .OneDModel
 
-run_OneDModel(:cabbeling, background_κz = 1e-2, convective_κz = 1.0, ν = 0.0, salinity_noise = true, Tᵤ = 0.5)
+run_OneDModel(:cabbeling, background_κz = 1e-2, convective_κz = 1.0, ν = 0.0, salinity_noise = true)
