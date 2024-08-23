6f70af18730763cc3a7b658b0c7d483ec7cf2ab3
jbisits <jbisits@gmail.com>
2024-08-23 16:39:26 +1000

    Les frequent checkpointing

--- cabbeling_gpu_salinitynoise.jl
+++ cabbeling_gpu_salinitynoise.jl
@@ -36,7 +36,7 @@ set_two_layer_initial_conditions!(tldns)
 max_Δt = 0.1
 stop_time = 20 * 60 * 60 # seconds
 save_schedule = 60  # seconds
-checkpointer_time_interval = 30 * 60 # seconds
+checkpointer_time_interval = 60 * 60 # seconds
 output_path = joinpath(@__DIR__, "outputs_equaldiffusion/")
 @info "Setting up simulation"
 simulation = TLDNS_simulation_setup(tldns, Δt, stop_time, save_schedule, TLDNS.save_computed_output!,
