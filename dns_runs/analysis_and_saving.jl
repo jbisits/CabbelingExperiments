ec0284667570c88b2d615b192e133cd4a70c4825
jbisits <jbisits@gmail.com>
2024-12-02 09:24:02 +1100

    Correct grid resolution for checkpoint check

--- dns_runs/analysis_and_saving.jl
+++ dns_runs/analysis_and_saving.jl
@@ -1,5 +1,9 @@
 using NCDatasets, StatsBase, JLD2, GibbsSeaWater
 
+tracers = "tracers.nc"
+computed_output = "computed_output.nc"
+velocities = "velocities.nc"
+
 """
     function effective_diffusivity!(computed_output::AbstractString, tracers::AbstractString)
 Compute the effective diffusivity for the salinity tracer `S` in `tracers`. The computed
@@ -42,11 +46,15 @@ function effective_diffusivity!(computed_output::AbstractString, tracers::Abstra
 
     # Save to computed output
     NCDataset(computed_output, "a") do ds
+
+        defDim(ds, "diff_z", length(Δz))
+        defDim(ds, "diff_t", length(Δt))
+
         defVar(ds, "Fₛ", κₛ, ("zC", diff_t),
                 attrib = ("longname" => "Horizontally averaged vertical salt flux"))
-        defVar(ds, "∂S∂z", κₛ, (diff_z, diff_t),
+        defVar(ds, "∂S∂z", ∂S∂z_save, (diff_z, diff_t),
                 attrib = ("longname" => "Horizontally averaged vertical salt gradient"))
-        defVar(ds, "κₛ", κₛ, (diff_z, diff_t),
+        defVar(ds, "κₛ", κₛ_save, (diff_z, diff_t),
                 attrib = ("longname" => "Horizontally averaged effective diffusivity"))
         defVar(ds, "∫κₛ", ∫κₛ, (diff_t,),
                 attrib = ("longname" => "Horizontally averaged depth integrated effective diffusivity"))
@@ -54,3 +62,72 @@ function effective_diffusivity!(computed_output::AbstractString, tracers::Abstra
 
     return nothing
 end
+
+"""
+    function potential_and_background_potential_energy!(computed_output::AbstractString)
+Compute and append the potential and background energy to `computed_output`. **Note** the
+PE and BPE are both referenced to ``z = 0``, and the saved quantities are volume integrated.
+"""
+function potential_and_background_potential_energy!(computed_output::AbstractString)
+
+    NCDataset(computed_output, "a") do ds
+
+        t = ds[:time][:]
+        SA = 0.1 * 0.1
+        Δx = diff(ds[:xC][1:2])[1]
+        Δy = diff(ds[:yC][1:2])[1]
+        Δz = diff(ds[:zC][1:2])[1]
+        ΔV = Δx * Δy * Δz
+
+        V = cumsum(ones(length(reshape(ds[:σ][:, :, :, 1], :)))) * ΔV
+        z✶ = V / SA
+
+        find_num = findfirst('k', ds.attrib["Reference density"]) - 1
+        ρ₀ = parse(Float64, ds.attrib["Reference density"][1:find_num])
+
+        σ = ds[:σ]
+        # background potential energy
+        Eb = similar(t)
+        g = 9.81
+        for i ∈ eachindex(t)
+            σᵢ = σ[:, :, :, i] .- ρ₀
+            σᵢ_array = reshape(σᵢ, :)
+            sort!(σᵢ_array, rev = true)
+            Eb[i] = (g / ρ₀) * sum(σᵢ_array .* z✶ * ΔV)
+        end
+
+        # save
+        defVar(ds, "∫Eb", Eb, (t,),
+                attrib = ("longname" => "Volume integrated background potential energy"))
+
+        # potential energy
+        z = ds[:zC]
+        z_ref0 = reverse(abs.(z))
+        z_grid = reshape(repeat(z_ref0, inner = 124 * 124), (124, 124, 1400))
+        Ep = similar(t)
+        for i ∈ eachindex(t)
+            σᵢ = σ[:, :, :, i] .- ρ₀
+            Ep[i] = (g / ρ₀) * sum(σᵢ .* z_grid * ΔV)
+        end
+
+    end
+
+    defVar(ds, "∫Ep", Ep, (t,),
+            attrib = ("longname" => "Volume integrated background potential energy"))
+
+    return nothing
+end
+"""
+    function extract_and_save!(saved_data::AbstractString, computed_output::AbstractString)
+Extract and save data needed for plots. The `saved_data` needs to be a `.jld2` file.
+"""
+function extract_and_save!(saved_data::AbstractString, computed_output::AbstractString, velocities::AbstractString)
+
+    return nothing
+end
+
+
+effective_diffusivity!(computed_output, tracers)
+potential_and_background_potential_energy!(computed_output)
+
+saved_data = "analysis_and_field_snapshots.jld2"
