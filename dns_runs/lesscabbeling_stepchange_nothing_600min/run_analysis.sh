156a8967c27ee7df9d4b54c952dfd56147d19001
jbisits <jbisits@gmail.com>
2025-01-13 14:20:08 +1100

    Fix file paths

--- dns_runs/lesscabbeling_stepchange_nothing_600min/run_analysis.sh
+++ dns_runs/lesscabbeling_stepchange_nothing_600min/run_analysis.sh
@@ -8,7 +8,7 @@
 #PBS -l wd
 #PBS -M z5131023@unsw.edu.au
 
-cd /g/data/e14/jb2381/CabbelingExperiments/dns_runs/cabbeling_stepchange_nothing_780min
+cd /g/data/e14/jb2381/CabbelingExperiments/dns_runs/lesscabbeling_stepchange_nothing_600min
 
 # Julia
 export JULIA_DEPOT_PATH="/g/data/e14/jb2381/.julia"
