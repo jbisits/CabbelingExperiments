fe318d59d2561604a2efc08ee4e35e758880ea3e
jbisits <jbisits@gmail.com>
2025-01-17 17:33:39 +1100

    Runnign scrtips

---
+++ dns_runs/run_unstable.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#PBS -q gpuvolta
+#PBS -P e14
+#PBS -l ncpus=12
+#PBS -l ngpus=1
+#PBS -l mem=96GB
+#PBS -l jobfs=1GB
+#PBS -l walltime=48:00:00
+#PBS -l storage=gdata/e14+scratch/e14
+#PBS -l wd
+#PBS -M z5131023@unsw.edu.au
+
+cd /g/data/e14/jb2381/CabbelingExperiments/dns_runs
+
+# Julia
+export JULIA_DEPOT_PATH="/g/data/e14/jb2381/.julia"
+export JULIA_NUM_THREADS=auto
+
+# Run the experiment
+julia --project dns_unstable_gpu_salinitynoise.jl > $PBS_JOBID.log
